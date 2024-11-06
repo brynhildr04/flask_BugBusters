f954448d174243627c9d43ad24294fb3ba52dcae
우민하 <minha2469@ewhain.net>
2024-11-07 08:58:20 +0900

    app.py 내용 수정 및 purchase, chat, cart(임의로 추가).html 연결

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template, request
+import sys
+application = Flask(__name__)
 
+@application.route("/")
+def productdetail():
+    return render_template("제품 상세 페이지.html")
+
+@application.route("/product-detail")
+def servicedetail():
+    return render_template("리뷰 상세 페이지.html")
+                
+if __name__=="__main__":
+    application.run(host='0.0.0.0', debug=True)
