f101ba0696db2fba3ad6e4e794a32b133d87325a
horokkk <smurfystrawberry@gmail.com>
2024-11-07 18:19:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request, url_for
 import os
 
-app = Flask(__name__)  # Flask 인스턴스 생성
+application = Flask(__name__)  # Flask 인스턴스 생성
 app.config['UPLOAD_FOLDER'] = 'static/images'
 
-@app.route("/")
+@application.route("/")
 def home():
     return render_template("상품등록하기.html")
 
-@app.route("/상품등록하기")
+@application.route("/상품등록하기.html")
 def view_register():
     return render_template("상품등록하기.html")
 
-@app.route("/submit_item_post", methods=['POST'])
+@application.route("/submit_item_post", methods=['POST'])
 def reg_item_submit_post():
     data = {
         "title": request.form.get("title", ""),
@@ -25,10 +25,10 @@ def reg_item_submit_post():
     if 'image' in request.files:
         image = request.files['image']
         if image.filename != '':
-            image.save(os.path.join(app.config['UPLOAD_FOLDER'], image.filename))
+            image.save(os.path.join(application.config['UPLOAD_FOLDER'], image.filename))
             img_path = url_for('static', filename='images/' + image.filename)
 
     return render_template("상품등록결과.html", data=data, img_path=img_path)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    application.run(host='0.0.0.0', debug=True)
