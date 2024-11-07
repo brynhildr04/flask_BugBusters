8f5c924f08da80e26ee2f7b08a8b3e3b18109857
horokkk <smurfystrawberry@gmail.com>
2024-11-07 18:11:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,35 @@
 from flask import Flask, render_template, redirect, url_for
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 # 기본 페이지 설정: purchase_p.html
-@app.route('/')
+@application.route('/')
 def home():
     return render_template('purchase_product.html')
 
 # 특정 버튼을 클릭하면 payment.html로 이동하는 경로 설정
-@app.route('/payment')
+@application.route('/payment.html')
 def view_payment():
     return render_template('payment.html')
 
 # 기타 독립적인 페이지
-@app.route('/aboutus')
+@application.route('/aboutus.html')
 def view_aboutus():
     return render_template('aboutus.html')
 
-@app.route('/chat_product')
+@application.route('/chat_product.html')
 def view_chat_product():
     return render_template('chat_product.html')
 
-@app.route('/chat_service')
+@application.route('/chat_service.html')
 def view_chat_service():
     return render_template('chat_service.html')
 
-@app.route('/purchase_service')
+@application.route('/purchase_service.html')
 def view_purchase_service():
     return render_template('purchase_service.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
 
